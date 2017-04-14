c306da46bdd6b3e2ea5e3870ecde234b599da935
IK <ik@develup.pro>
2017-04-14 12:38:12 +0300

    upd

--- frontend/controllers/api/v1/PostsController.php
+++ frontend/controllers/api/v1/PostsController.php
@@ -2,9 +2,24 @@
 
 namespace frontend\controllers\api\v1;
 
+use common\models\PostSearch;
 use yii\rest\ActiveController;
 
 class PostsController extends ActiveController
 {
     public $modelClass = 'common\models\Post';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+        return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+        $searchModel = new PostSearch();
+        return $searchModel->search(\Yii::$app->request->queryParams);
+    }
+
 }
